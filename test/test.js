4c1bb7e4ff8d32f4f49a029a0125456bce307cd9
Bill Yu <han.yu@duke.edu>
2018-05-06 18:32:07 -0400

    test setup and add travis and coveralls integration

---
+++ test/test.js
@@ -0,0 +1,26 @@
+const chai = global.chai = require('chai');
+const chaiHttp = require('chai-http');
+process.env.MONGO_URL = 'mongodb://localhost:27017/test';
+global.server = require('../server/server');
+global.should = chai.should();
+global.assert = chai.assert;
+chai.use(chaiHttp);
+
+const mongoose = require('mongoose');
+
+before((done) => {
+  mongoose.connect(process.env.MONGO_URL)
+    .then(() => mongoose.connection.db.dropDatabase(done))
+    .catch(err => done(err));
+});
+
+describe('404', () => {
+  it('should 404 for undefined routes', (done) => {
+    chai.request(server)
+      .get('/wewillneverusethisroute')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
+});
